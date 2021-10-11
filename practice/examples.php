824f9011e439434cc54e30041e513183c75e2722
Porsha Lurry <plurry48@gmail.com>
2021-10-11 16:39:01 -0400

    updates

--- practice/examples.php
+++ practice/examples.php
@@ -1,9 +1,18 @@
 <?php
- class Phone {
+ class Car {
     public $color;
     public $manufacturer;
-    static public $numberSold = 13;
     }
-    Phone::$numberSold+=5;
-    echo Phone::$numberSold;
+    $beetle = new Car();
+    $beetle->color = "red";
+    $beetle->manufacturer = "Volkswagen";
+    $mustang = new Car();
+    $mustang->color = "green";
+    $mustang->manufacturer = "Ford";
+    echo $beetle->color."\n";
+    echo $beetle->manufacturer."\n";
+    echo $mustang->color."\n";
+    echo $mustang->manufacturer."\n";
+    print_r($beetle);
+    print_r($mustang);
 ?>
---
+++ practice/examples/examplespart.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <title>Table</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+     <form method ="POST" action="examplespart.php">
+     <input type="text" name="firstname">
+     <?php echo $_POST["firstname"];?>
+    
+
+    </body>
+</html>
+
+
